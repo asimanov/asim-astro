3c7fefdc0329a11bd5630e1479f22ee405bb70ec
Anton Simanov <anton.simanov@gmail.com>
2022-10-09 17:22:07 -0700

    re-organized asset location, built out about page

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Anton Simanov';
+export const SITE_TITLE = 'ASIM';
+export const AUTHOR_NAME = 'Anton Simanov';
 export const SITE_DESCRIPTION = 'Digital designer and web developer from the San Joaquin Valley';
