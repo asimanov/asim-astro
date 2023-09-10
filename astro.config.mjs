6c3b0fc31277b91c4e0726b772785ac839535bbb
Anton Simanov <anton.simanov@gmail.com>
2023-09-09 19:42:04 -0700

    slight content edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,18 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		render: [
+		  '@astrojs/markdown-remark',
+		  {
+			syntaxHighlight: 'shiki',
+			shikiConfig: {
+			  theme: 'monokai',
+			  langs: ['js', 'html', 'css', 'astro',],
+			  wrap: false,
+			},
+		  },
+		],
+	  },
 });
+
